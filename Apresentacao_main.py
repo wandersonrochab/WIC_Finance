a279981701aaefbea1b90539ddc64e4d5b2ee718
Wanderson Bittencourt <31345437+wandersonrochab@users.noreply.github.com>
2024-08-04 15:33:39 -0300

    Add files via upload

--- Apresentacao_main.py
+++ Apresentacao_main.py
@@ -5,6 +5,11 @@ import pandas as pd
 import os
 
 # importar os arquivos das paginas individuais
+from code.panorama import Panorama
+from code.apresentacao import display_app
+
+from code.funct_aux import Filter
+from code.dataset import ImportarDados
 # import panorama
 # import receitas
 # import despesas
@@ -15,14 +20,14 @@ import os
 # import funcoes
 
 
-@st.cache_data
-def ImportarDados():
-    df = pd.read_excel('base.xlsx')
-    df['Ano'] = df['Data'].dt.strftime('%Y')
-    df['Tipo'] = df['Valor $'].apply(lambda x: 'R' if x > 0 else 'D')
-    # print(df)
+# @st.cache_data
+# def ImportarDados():
+#     df = pd.read_excel('base.xlsx')
+#     df['Ano'] = df['Data'].dt.strftime('%Y')
+#     df['Tipo'] = df['Valor $'].apply(lambda x: 'R' if x > 0 else 'D')
+#     # print(df)
 
-    return df
+#     return df
 
 class Apresentacao():
     def __init__(self) -> None:
@@ -33,7 +38,7 @@ class Apresentacao():
         with st.sidebar:
             st.sidebar.image(self.logo, use_column_width = True)
             choose = option_menu('MENU',
-                         ['Lembretes', 'Panorama', 'Receitas', 'Despesas', 
+                         ['Apresentação', 'Panorama', 'Receitas', 'Despesas', 
                           'DRE', "DFC", 'Orçamento', 'Pessoal'],
                          icons = ['bookmark-check', 'cash','coin', 'wallet2', #https://icons.getbootstrap.com/?q=person
                                           'cash','cash', 'eye','person'],
@@ -47,46 +52,45 @@ class Apresentacao():
                                                      "--hover-color": "#427aa1"},
                                         "nav-link-selected": {"background-color": "#031d44"}}
                         )
-        # with open('style.css') as f: # retirar os rodape da apresentacao
-        #     st.markdown(f'<style>{f.read()}</style>',
-        #                  unsafe_allow_html = True)
-        n_empresas = os.listdir('Imagens/empresas')
-        c = st.columns(len(n_empresas))
-        with c[0]:
-            st.image(os.path.join('Imagens/empresas', n_empresas[0]), width=250)
-        with c[1]:
-            st.image(os.path.join('Imagens/empresas', n_empresas[1]), width=250)
-        with c[2]:
-            st.image(os.path.join('Imagens/empresas', n_empresas[2]), width=250)
-        with c[3]:
-            st.image(os.path.join('Imagens/empresas', n_empresas[3]), width=250)
-        with c[4]:
-            st.image(os.path.join('Imagens/empresas', n_empresas[4]), width=250)
-        
+        if choose != 'Apresentação':
+            n_empresas = os.listdir('Imagens/empresas')
+            c = st.columns(len(n_empresas))
+            with c[0]:
+                st.image(os.path.join('Imagens/empresas', n_empresas[0]), width=250)
+            with c[1]:
+                st.image(os.path.join('Imagens/empresas', n_empresas[1]), width=250)
+            with c[2]:
+                st.image(os.path.join('Imagens/empresas', n_empresas[2]), width=250)
+            with c[3]:
+                st.image(os.path.join('Imagens/empresas', n_empresas[3]), width=250)
+            with c[4]:
+                st.image(os.path.join('Imagens/empresas', n_empresas[4]), width=250)
+            
 
-        # def Selecao():
-        #     col = st.columns(2)
-        #     with col[0]:
-        #         anos = st.multiselect('Selecione um período!', 
-        #                             funcoes.Filtro(ImportarDados(), 'Ano', 'vazia'),
-        #                             default = sorted(funcoes.Filtro(ImportarDados(), 
-        #                                                             'Ano', 'vazia'))[-1]
-        #                             )
-                
-        #     with col[1]:
-        #         empresa = st.multiselect('Selecione uma empresa!', 
-        #                             funcoes.Filtro(ImportarDados(), 'Empresa', 'vazia'),
-        #                             default =sorted(funcoes.Filtro(ImportarDados(), 
-        #                                                             'Empresa', 'vazia'))[0]
-        #                             )
-        #     return anos, empresa          
-        
-        
-        # anos, empresa = Selecao()
-        # if choose == 'Lembretes':
-        #     pass
-        # elif choose == 'Panorama':
-        #     panorama.Panorama(anos, empresa)
+            def Selecao():
+                col = st.columns(2)
+                with col[0]:
+                    anos = st.multiselect('Selecione um período!', 
+                                        sorted(Filter(ImportarDados(), 'Ano')),
+                                        default = sorted(Filter(ImportarDados(), 
+                                                                        'Ano'))[-1]
+                                        )
+                    
+                with col[1]:
+                    empresa = st.multiselect('Selecione uma empresa!', 
+                                        Filter(ImportarDados(), 'Empresa'),
+                                        default =sorted(Filter(ImportarDados(), 
+                                                                        'Empresa'))[0]
+                                        )
+                return anos, empresa          
+            
+            lts = {1:'Valor $'}
+            anos, empresa = Selecao()
+            
+        if choose == 'Apresentação':
+            display_app()
+        elif choose == 'Panorama':
+            Panorama(anos, empresa, lts)
         # elif choose == 'Receitas':
         #     receitas.Receitas(anos, empresa)
         # elif choose == 'Despesas':
@@ -99,13 +103,12 @@ class Apresentacao():
         #     orcamento.Orcamento()
         # elif choose == 'Pessoal':
         #     pessoas.Pessoas()
-        # else:
-        #     pass
+        else:
+            pass
 
 
 
-
-
-funcao = Apresentacao()
-funcao.Modelo()
+if __name__ == "__main__":
+    funcao = Apresentacao()
+    funcao.Modelo()
         
